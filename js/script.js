ea696b5d85c6614187af7368961a3f15a9e9d79c
Isaac <uniqucode@gmail.com>
2022-09-07 09:35:24 +0100

    Refrencing radio inputs

--- js/script.js
+++ js/script.js
@@ -6,3 +6,4 @@ const countdownPage = document.getElementById('countdown-page');
 // Splash Page
 const startForm = document.getElementById('start-form');
 const radioContainers = document.querySelectorAll('.radio-container');
+const radioInputs = document.querySelectorAll('input');
