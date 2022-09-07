b42c72fb7ab667edff1aadc6de45b7ccd3481234
Isaac <uniqucode@gmail.com>
2022-09-07 09:30:41 +0100

    Refrencing splash page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 // Pages
 const gamePage = document.getElementById('game-page');
 const scorePage = document.getElementById('score-page');
+const splashPage = document.getElementById('splash-page');
