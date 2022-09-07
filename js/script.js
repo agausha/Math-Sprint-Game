6027ca4a7a43361b29bc57c7602f747eb3d2f68d
Isaac <uniqucode@gmail.com>
2022-09-07 18:32:50 +0100

    Created a function

--- js/script.js
+++ js/script.js
@@ -81,6 +81,9 @@ function equationsToDOM() {
   });
 }
 
+// Dynamically adding correct/incorrect equations
+function populateGamePage() {}
+
 
 // Displays 5, 4, 3, 2, 1, GO!
 function countdownStart() {
