989d231df780108265fac54c9ca285173afe9073
Isaac <uniqucode@gmail.com>
2022-09-07 09:31:56 +0100

    Refrencing countdown page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@
 const gamePage = document.getElementById('game-page');
 const scorePage = document.getElementById('score-page');
 const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
